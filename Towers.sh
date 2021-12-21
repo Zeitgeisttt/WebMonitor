d4f3cce9af936d51459b070c421f175179b404a1
Junwei Chen <42275000+Zeitgeisttt@users.noreply.github.com>
2021-12-21 02:52:36 -0800

    Embedded the shell file inside a folder

--- Towers.sh
+++ Towers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #check Towers house every 30 seconds
-while true; do python3.7 websitechanges.py --folder ./TWERS/ --url https://www.on-site.com/apply/property/1167 --to cjunwei724@163.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
+while true; do python3.7 webmonitor.py --folder ./ --url https://www.on-site.com/apply/property/1167 --to cjunwei724@163.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
 
