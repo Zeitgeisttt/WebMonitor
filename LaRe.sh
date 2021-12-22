761cfae73f65d60a5f1fa1dfea4411d7584880be
Junwei Chen <42275000+Zeitgeisttt@users.noreply.github.com>
2021-12-22 15:54:51 -0800

    Embedded directory

--- LaRe.sh
+++ LaRe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #check La Regencia house every 30 seconds
-while true; do python3.7 websitechanges.py --folder ./LRegencia/ --url https://www.on-site.com/apply/property/1360 --to cjunwei724@163.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
+while true; do python3.7 ../webmonitor.py --folder ./ --url https://www.on-site.com/apply/property/1360 --to example@xxx.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
