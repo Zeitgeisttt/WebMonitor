2facb0992100e932da953c3478cdb324ae321571
Junwei Chen <42275000+Zeitgeisttt@users.noreply.github.com>
2021-12-22 15:51:28 -0800

    Embedded CostaVerde.sh in a directory

---
+++ CostaVerde/CostaVerde.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#check CVV house every 30 seconds
+while true; do python3.7 ../webmonitor.py --folder ./ --url https://www.on-site.com/apply/property/1166 --to example@xxx.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
