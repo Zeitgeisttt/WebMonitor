8aa52a1443082ca38a13398a277abb8632497c0b
Junwei Chen <42275000+Zeitgeisttt@users.noreply.github.com>
2021-12-22 16:42:51 -0800

    Modified python version

--- LaRegencia/LaRegencia.sh
+++ LaRegencia/LaRegencia.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #check La Regencia house every 30 seconds
-while true; do python3.7 ../webmonitor.py --folder ./ --url https://www.on-site.com/apply/property/1360 --to example@xxx.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
+while true; do python ../webmonitor.py --folder ./ --url https://www.on-site.com/apply/property/1360 --to example@xxx.com --smtpemail newhouseavailable@163.com --smtppass WXCJKVYMVHXDGAKX --threshold 0.95; sleep 30; done
